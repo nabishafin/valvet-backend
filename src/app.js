c0509e8ad5550b7278c54663c7824f8f560b29af
nabishafin <shafin21215@gmail.com>
2026-06-17 00:51:40 +0600

    urls added on the allow origin

--- src/app.js
+++ src/app.js
@@ -15,6 +15,8 @@ const allowedOrigins = [
   'http://localhost:3001',
   'http://localhost:3002',
   'http://localhost:8000',
+  "https://velvetrougesalonsuites.com",
+  "https://admin.velvetrougesalonsuites.com"
 ]
 
 app.use(helmet())
